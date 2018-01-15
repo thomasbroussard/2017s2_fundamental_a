84c2049115bd6c69d1748e4776201506abd30562
tbrou <tbrou@tbrou-PC7.mshome.net>
2018-01-15 19:22:03 +0100

    * insert DB test

--- iam-core-ref/readme.md
+++ iam-core-ref/readme.md
@@ -7,4 +7,3 @@
 
 # TODO (2017/11/27)
 - Complete the create method from IdentityFileDAO so that it concretely writes in the identities.txt file
-
--- iam-core-ref/test/fr/epita/iam/services/test/TestDB.java
+++ iam-core-ref/test/fr/epita/iam/services/test/TestDB.java
@@ -57,13 +57,79 @@ public class TestDB {
 	 * @throws SQLException
 	 */
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		final String url = "jdbc:derby://localhost:1527/iam;create=true";
-		final String password = "root";
-		final String username = "root";
+		// testSelectQuery();
 
-		Class.forName("org.apache.derby.jdbc.ClientDriver");
+		// differenceBetweenPreparedStatementAndStatement();
 
-		final Connection connection = DriverManager.getConnection(url, username, password);
+		final Connection connection = getConnection();
+		final PreparedStatement pstmt = connection.prepareStatement("INSERT INTO IDENTITIES(UID, EMAIL, DISPLAY_NAME) VALUES (?, ?, ?)");
+		pstmt.setString(1, "4567");
+		pstmt.setString(2, "cserr@cserr.com");
+		pstmt.setString(3, "Clément");
+
+		pstmt.execute();
+
+		connection.close();
+	}
+
+	/**
+	 * <h3>Description</h3>
+	 * <p>
+	 * This methods allows to ...
+	 * </p>
+	 *
+	 * <h3>Usage</h3>
+	 * <p>
+	 * It should be used as follows :
+	 *
+	 * <pre>
+	 * <code> ${enclosing_type} sample;
+	 *
+	 * //...
+	 *
+	 * sample.${enclosing_method}();
+	 *</code>
+	 * </pre>
+	 * </p>
+	 *
+	 * @since $${version}
+	 * @see Voir aussi $${link}
+	 * @author ${user}
+	 *
+	 *         ${tags}
+	 */
+	private static void differenceBetweenPreparedStatementAndStatement() throws ClassNotFoundException, SQLException {
+		final Connection connection = getConnection();
+		final PreparedStatement prepareStatement = connection.prepareStatement("select * from IDENTITIES where DISPLAY_NAME = ?");
+		// Don't do what follows, this will not be protected against SQL injections.
+		// Statement statement = connection.createStatement("select * from IDENTITIES where DISPLAY_NAME = ?");
+		final String parameter = " 'toto' and 1 = 1; DROP TABLE IDENTIES";
+		prepareStatement.setString(1, parameter);
+	}
+
+	/**
+	 * <h3>Description</h3>
+	 * <p>This methods allows to ...</p>
+	 *
+	 * <h3>Usage</h3>
+	 * <p>It should be used as follows :
+	 *
+	 * <pre><code> ${enclosing_type} sample;
+	 *
+	 * //...
+	 *
+	 * sample.${enclosing_method}();
+	 *</code></pre>
+	 * </p>
+	 *
+	 * @since $${version}
+	 * @see Voir aussi $${link}
+	 * @author ${user}
+	 *
+	 * ${tags}
+	 */
+	private static void testSelectQuery() throws ClassNotFoundException, SQLException {
+		final Connection connection = getConnection();
 
 		final PreparedStatement ptst = connection.prepareStatement("select DISPLAY_NAME from IDENTITIES");
 		final ResultSet rs = ptst.executeQuery();
@@ -76,7 +142,38 @@ public class TestDB {
 		rs.close();
 		ptst.close();
 		connection.close();
+	}
 
+	/**
+	 * <h3>Description</h3>
+	 * <p>This methods allows to ...</p>
+	 *
+	 * <h3>Usage</h3>
+	 * <p>It should be used as follows :
+	 *
+	 * <pre><code> ${enclosing_type} sample;
+	 *
+	 * //...
+	 *
+	 * sample.${enclosing_method}();
+	 *</code></pre>
+	 * </p>
+	 *
+	 * @since $${version}
+	 * @see Voir aussi $${link}
+	 * @author ${user}
+	 *
+	 * ${tags}
+	 */
+	private static Connection getConnection() throws ClassNotFoundException, SQLException {
+		final String url = "jdbc:derby://localhost:1527/iam;create=true";
+		final String password = "root";
+		final String username = "root";
+
+		Class.forName("org.apache.derby.jdbc.ClientDriver");
+
+		final Connection connection = DriverManager.getConnection(url, username, password);
+		return connection;
 	}
 
 }
