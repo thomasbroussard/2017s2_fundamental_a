8972b12233a5a44e0322be219be1e28144216e17
tbrou <tbrou@tbrou-PC7.natsystem.fr>
2018-01-30 20:11:46 +0100

    * Xml integration

---
+++ iam-core-ref/src/fr/epita/iam/services/IdentityXMLDAO.java
@@ -0,0 +1,214 @@
+/**
+ * Ce fichier est la propriété de Thomas BROUSSARD Code application : Composant :
+ */
+package fr.epita.iam.services;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import fr.epita.iam.datamodel.Identity;
+import fr.epita.iam.exceptions.IdentityCreationException;
+import fr.epita.iam.exceptions.IdentitySearchException;
+import fr.epita.iam.services.configuration.ConfigurationService;
+import fr.epita.iam.services.dao.IdentityDAO;
+import fr.epita.logger.Logger;
+
+/**
+ * <h3>Description</h3>
+ * <p>
+ * This class allows to ...
+ * </p>
+ *
+ * <h3>Usage</h3>
+ * <p>
+ * This class should be used as follows:
+ *
+ * <pre>
+ * <code>${type_name} instance = new ${type_name}();</code>
+ * </pre>
+ * </p>
+ *
+ * @since $${version}
+ * @see See also $${link}
+ * @author ${user}
+ *
+ *         ${tags}
+ */
+public class IdentityXMLDAO implements IdentityDAO {
+
+	private static final Logger LOGGER = new Logger(IdentityXMLDAO.class);
+
+	/**
+	 *
+	 */
+	private static final String PROPERTY = "property";
+	/**
+	 *
+	 */
+	private static final String IDENTITY = "identity";
+	private Document document;
+
+	/**
+	 *
+	 */
+	public IdentityXMLDAO() {
+		try {
+			final ConfigurationService configuration = ConfigurationService.getInstance();
+			final File file = new File(configuration.getConfigurationValue("xml.file.path"));
+			final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			final DocumentBuilder documentBuilder = dbf.newDocumentBuilder();
+
+			if (file.exists()) {
+
+				document = documentBuilder.parse(new FileInputStream(file));
+
+			} else {
+				document = documentBuilder.newDocument();
+
+				document.appendChild(document.createElement("identities"));
+			}
+		} catch (SAXException | IOException | ParserConfigurationException e) {
+
+			LOGGER.error("An error occured", e);
+		}
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see fr.epita.iam.services.dao.IdentityDAO#create(fr.epita.iam.datamodel.Identity)
+	 */
+	@Override
+	public void create(Identity identity) throws IdentityCreationException {
+		final Element root = document.getDocumentElement();
+		final Element identityElement = getNewIdentityElt();
+		getNewPropertyElmt("displayName", identity.getDisplayName());
+		getNewPropertyElmt("uid", identity.getUid());
+		getNewPropertyElmt("email", identity.getEmail());
+		root.appendChild(identityElement);
+
+	}
+
+	/**
+	 * <h3>Description</h3>
+	 * <p>
+	 * This methods allows to ...
+	 * </p>
+	 *
+	 * <h3>Usage</h3>
+	 * <p>
+	 * It should be used as follows :
+	 *
+	 * <pre>
+	 * <code> ${enclosing_type} sample;
+	 *
+	 * //...
+	 *
+	 * sample.${enclosing_method}();
+	 *</code>
+	 * </pre>
+	 * </p>
+	 *
+	 * @since $${version}
+	 * @see Voir aussi $${link}
+	 * @author ${user}
+	 *
+	 *         ${tags}
+	 */
+	private void getNewPropertyElmt(String propertyName, String propertyValue) {
+		final Element identityProperty = getNewPropertyElt();
+		identityProperty.setNodeValue(propertyName);
+		identityProperty.setAttribute(propertyName, propertyValue);
+		identityProperty.setTextContent(propertyValue);
+	}
+
+	private Element getNewIdentityElt() {
+		return document.createElement(IDENTITY);
+	}
+
+	private Attr getNewNameAttr() {
+		return document.createAttribute("name");
+	}
+
+	private Element getNewPropertyElt() {
+		return document.createElement(PROPERTY);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see fr.epita.iam.services.dao.IdentityDAO#search(fr.epita.iam.datamodel.Identity)
+	 */
+	@Override
+	public List<Identity> search(Identity criteria) throws IdentitySearchException {
+		final List<Identity> identities = new ArrayList<>();
+		final NodeList list = document.getElementsByTagName(IDENTITY);
+
+		final int length = list.getLength();
+		for (int i = 0; i < length; i++) {
+			final Node identityNode = list.item(i);
+			if (identityNode instanceof Element) {
+				final Element identityElement = (Element) list.item(i);
+				final Identity identity = new Identity();
+				final NodeList propertiesList = identityElement.getElementsByTagName(PROPERTY);
+				final int propertiesLength = propertiesList.getLength();
+				for (int j = 0; j < propertiesLength; j++) {
+					final Node propertyNode = propertiesList.item(j);
+					if (propertyNode instanceof Element) {
+						final Element propertyElement = (Element) propertyNode;
+						final String attribute = propertyElement.getAttribute("name");
+						switch (attribute) {
+						case "displayName":
+							identity.setDisplayName(propertyElement.getTextContent());
+							break;
+						case "email":
+							identity.setEmail(propertyElement.getTextContent());
+							break;
+						case "uid":
+							identity.setUid(propertyElement.getTextContent());
+							break;
+
+						}
+					}
+				}
+				identities.add(identity);
+			}
+		}
+		return identities;
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see fr.epita.iam.services.dao.IdentityDAO#update(fr.epita.iam.datamodel.Identity)
+	 */
+	@Override
+	public void update(Identity identity) {
+		// TODO Complete this stub or remove that comment line
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see fr.epita.iam.services.dao.IdentityDAO#delete(fr.epita.iam.datamodel.Identity)
+	 */
+	@Override
+	public void delete(Identity identity) {
+		// TODO Complete this stub or remove that comment line
+
+	}
+
+}
---
+++ iam-core-ref/test/fr/epita/iam/services/test/TestIdentityXMLDAO.java
@@ -0,0 +1,80 @@
+/**
+ * Ce fichier est la propriété de Thomas BROUSSARD
+ * Code application :
+ * Composant :
+ */
+package fr.epita.iam.services.test;
+
+import java.util.List;
+
+import fr.epita.iam.datamodel.Identity;
+import fr.epita.iam.exceptions.IdentityCreationException;
+import fr.epita.iam.exceptions.IdentitySearchException;
+import fr.epita.iam.services.IdentityXMLDAO;
+import fr.epita.iam.services.dao.IdentityDAO;
+
+/**
+ * <h3>Description</h3>
+ * <p>This class allows to ...</p>
+ *
+ * <h3>Usage</h3>
+ * <p>This class should be used as follows:
+ *   <pre><code>${type_name} instance = new ${type_name}();</code></pre>
+ * </p>
+ *
+ * @since $${version}
+ * @see See also $${link}
+ * @author ${user}
+ *
+ * ${tags}
+ */
+public class TestIdentityXMLDAO {
+
+	/**
+	 * <h3>Description</h3>
+	 * <p>
+	 * This methods allows to ...
+	 * </p>
+	 *
+	 * <h3>Usage</h3>
+	 * <p>
+	 * It should be used as follows :
+	 *
+	 * <pre>
+	 * <code> ${enclosing_type} sample;
+	 *
+	 * //...
+	 *
+	 * sample.${enclosing_method}();
+	 *</code>
+	 * </pre>
+	 * </p>
+	 *
+	 * @since $${version}
+	 * @see Voir aussi $${link}
+	 * @author ${user}
+	 *
+	 *         ${tags}
+	 * @throws IdentityCreationException
+	 * @throws IdentitySearchException
+	 */
+	public static void main(String[] args) throws IdentityCreationException, IdentitySearchException {
+		// given
+		final IdentityDAO dao = new IdentityXMLDAO();
+
+		// when
+		dao.create(new Identity("Thomas Broussard", "1234", "tbr@tbr.com"));
+
+		// then
+		final List<Identity> list = dao.search(new Identity("Thomas", null, null));
+
+		if (list.isEmpty()) {
+			System.out.println("failure");
+		} else {
+			System.out.println("success");
+		}
+		System.out.println(list);
+
+	}
+
+}
